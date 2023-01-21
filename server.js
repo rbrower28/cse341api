421ee5b1108d82e46a54510a5304c951efb64198
Ryan <ryan.brower28@gmail.com>
2023-01-21 13:21:50 -0700

    CRUD integration

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 const mongodb = require('./db/connect');
+const bodyParser = require('body-parser');
 
-app.use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
